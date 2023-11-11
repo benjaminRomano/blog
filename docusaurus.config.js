59833c18a03f5ba5836dfeb47249a656a97c042b
Benjamin Romano <bromano2100@gmail.com>
2023-11-11 02:20:43 -0800

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "benjaminromano", // Usually your GitHub org/user name.
   projectName: "blog", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
